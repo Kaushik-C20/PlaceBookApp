468d6bbe3c8719a528cabe28244a6ddd3a486367
Kaushik C <58183296+Kaushik-C20@users.noreply.github.com>
2020-08-02 16:00:22 +0530

    API key works only for few IP addresses

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -37,7 +37,7 @@ export class NewOfferPage implements OnInit {
         updateOn: 'blur',
         validators: [Validators.required]
       }),
-      location: new FormControl(null)
+      location: new FormControl(null, {validators: [Validators.required]})
     });
   }
 
